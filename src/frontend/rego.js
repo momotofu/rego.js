bed16c4a94dcb6acf4520157e9398853dac55a26
Christopher Reece <mrkita.reece@gmail.com>
2018-11-06 20:57:24 +0900

    added isMounted check and semicolons

--- src/frontend/rego.js
+++ src/frontend/rego.js
@@ -5,29 +5,32 @@ window.rego = (function() {
       // TODO: work out props logic
       // TODO: add meaningful and descriptive comments
 
-      var state = initialState
-      var props = initialProps
+      var isMounted = false;
+      var state = initialState;
+      var props = initialProps;
 
       var blockMethods = {
         setState: function(newState) {
-          state = Object.assign({}, state, newState)
-          this.render()
+          state = Object.assign({}, state, newState);
+          this.render();
         },
 
         getState: function() {
-          return state
+          return state;
         },
 
         mount: function() {
-          DOMLogic.mount.call(this, state, props)
+          isMounted = true;
+          DOMLogic.mount.call(this, state, props);
         },
 
         unmount: function() {
-          DOMLogic.unmount.call(this)
+          isMounted = false;
+          DOMLogic.unmount.call(this);
         },
 
         render: function() {
-          DOMLogic.render.call(this, state, props)
+          DOMLogic.render.call(this, state, props);
         }
       }
 
